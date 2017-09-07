68d9cc1c69e47fac2b54775265dc16adf7d84d9b
Tomasz Belina <tomasz.belina@qualent.eu>
2017-09-07 23:44:23 +0200

    dodaj testy atr

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -1,9 +1,9 @@
 import HomeModule from './home.module';
 
 fdescribe('home', () => {
-    let server, app, $scope, $state, Notifications;
+    let server, app, $scope, $state, Notifications, homeCtrl;
     let searchService
-    const {expectElement, type} = testRunner.actions;
+    const {expectElement, type, click} = testRunner.actions;
 
     beforeEach(() => {
 
@@ -11,11 +11,18 @@ fdescribe('home', () => {
 
         // wtrzyknięcie zależności jezeli będą potrzebne
         angular.module(module)
-            .run((_$rootScope_, _$state_, _SearchService_,_Notifications_) => {
+            .run((_$rootScope_, _$state_, _SearchService_,_Notifications_, _$controller_) => {
                 $scope = _$rootScope_.$new();
                 $state = _$state_;
                 searchService = _SearchService_;
                 Notifications = _Notifications_;
+
+                homeCtrl = _$controller_('HomeCtrl', {
+                    $state,
+                    $scope,
+                    searchService,
+                    Notifications
+                });
         });
 
         app = testRunner.app([module, 'templates']);
@@ -54,6 +61,52 @@ fdescribe('home', () => {
         expect(searchService.showMe.calls.count()).toBe(1); 
        
     });
+
+    function getFakeSearchResult() {
+      return [
+      {
+        league: 443,
+        caption: 'garfield'
+      },
+      {
+        league: 444,
+        caption: 'garfield'
+      }];
+    }
+
+    // TODO TB: nie wiem jak podpiac sie pod zdarzenie ng-click na liscie elementow li
+    // Error: Could not find li:first.items element for click action
+    it('err1', () => {
+        
+        const html = app.runHtml('<home-component></home-component>');
+        
+        spyOn(Notifications, 'showToastNotification');
+
+        homeCtrl.itemList = getFakeSearchResult();
+
+        html.perform(
+            click.in('li:first.items')
+        );
+
+        expect(Notifications.showToastNotification)
+            .toHaveBeenCalledWith('Something goes wrong, try again later');   
+    });
+
+    // TODO TB: jak sprawdzic ze zostaly w html utworzone 2 elementy li
+    it('err2', () => {
+        
+        const html = app.runHtml('<home-component></home-component>');
+        
+        homeCtrl.itemList = getFakeSearchResult();
+
+        html.verify(
+            expectElement('ul li.items').toHaveText('garfield')
+        )
+
+    });
+
+
+
 
 
 });
